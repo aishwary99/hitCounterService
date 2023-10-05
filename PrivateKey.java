5d5ed2d94a1c6e28ebfb68bde333e12d6fbac6d3
aishwary99 <aishwaryadwani.9799@gmail.com>
2023-10-05 12:54:24 -0400

    MultipleClients | Housekeeping work

---
+++ PrivateKey.java
@@ -0,0 +1,14 @@
+public class PrivateKey {
+    private Set<String> keys;
+    
+    public PrivateKey() {
+    }
+
+    public void setKeys(Set<String> keys) {
+        this.keys = keys;
+    }
+
+    public Set<String> getKeys() {
+        return this.keys;
+    }
+}
